0dc936f86c195e7c36e47a13e78d46bd0262d470
Tom Mowad <tmowad@gmail.com>
2013-07-10 05:46:27 +0000

    clarity bro

--- web.js
+++ web.js
@@ -5,15 +5,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  console.warn("pre-first");
-  response.send('screw you godiva');
-  console.warn("post-first");
-  var buf = fs.readFileSync('index.html');
-  console.warn("post-read");
   response.send(buf.toString());
-  console.warn("post-print");
-  response.send('screw you again...');
-  console.warn("post-second");
 });
 
 var port = process.env.PORT || 5000;
