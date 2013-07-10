17b4a949d1a175449abe798952acddd29163a6c3
Tom Mowad <tmowad@gmail.com>
2013-07-10 05:02:48 +0000

    Modified web.js to print out contents of index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var contents = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
+  response.send(contents);
 });
 
 var port = process.env.PORT || 5000;
