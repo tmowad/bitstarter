1de104782ebe2546b2c07a661d02ca5079ef4616
Tom Mowad <tmowad@gmail.com>
2013-07-10 05:44:37 +0000

    godiva sanity

--- web.js
+++ web.js
@@ -5,10 +5,15 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('screw you');
+  console.warn("pre-first");
+  response.send('screw you godiva');
+  console.warn("post-first");
   var buf = fs.readFileSync('index.html');
+  console.warn("post-read");
   response.send(buf.toString());
+  console.warn("post-print");
   response.send('screw you again...');
+  console.warn("post-second");
 });
 
 var port = process.env.PORT || 5000;
